1ec317bb12b828e6600c60ccb52e6d80d7438215
cnxw4570123 <cnxw4570123@gmail.com>
2025-06-09 22:11:41 +0900

    init

---
+++ src/main/java/com/sparta/mms/MmsApplication.java
@@ -0,0 +1,12 @@
+package com.sparta.mms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmsApplication.class, args);
+    }
+}
